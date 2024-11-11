3b5033f756f75070f133006c2518896c779c502d
Thiên Nguyễn <broagii@yahoo.com>
2024-11-11 16:57:25 +0700

    add taxonomy service cat

---
+++ wp-content/themes/flatsome-child/taxonomy-service_cat.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The blog template file.
+ *
+ * @package flatsome
+ */
+
+ $cat = get_queried_object()->term_id;
+
+get_header();
+
+?>
+
+<div id="content" class="">
+
+<section class="section section-padding">
+<div class="section-content relative">
+
+  <div class="row">
+  <div class="pd0 medium-6 small-12">
+  <div class="col-inner text-left">
+
+
+      <div class="title-lg"><h2><?php echo get_queried_object()->name; ?></h2></div>
+          </div>
+  </div>
+  <?php echo do_shortcode('[service_post max="" id="'.get_queried_object()->term_id.'"]'); ?>
+  </div>
+</div>
+</section>
+
+<?php
+			do_action('flatsome_after_blog');
+			?>
+</div>
+
+<?php get_footer();
